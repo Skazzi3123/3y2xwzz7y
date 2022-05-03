a0deea149ab714246ea440f5f1104247acb07bc1
Skazzi3123 <98550858+Skazzi3123@users.noreply.github.com>
2022-05-03 19:01:07 +0200

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,53 @@
+local delta = game:HttpGet("https://pastebin.com/raw/SmD5UViR")
+local ui = game:HttpGet("https://pastebin.com/raw/FQLiP9Ps")
+
+writefile("ui_nxwr.lua", ui)
+
+local games = {
+    ['rushpoint'] = {2162282815};
+    ['state of anarchy'] = {595270616};
+    ['project delta'] = {2862098693};
+}
+
+if not isfolder('nxwr_games') then
+    makefolder('nxwr_games');
+    writefile("nxwr_games/project delta.lua", delta)
+end
+
+repeat task.wait() until game:IsLoaded();
+
+local filename = 'nxwr_games';
+local files = {};
+
+for i,v in next, listfiles(filename) do
+    files[v:gsub(filename..'\\','')] = readfile(v);
+end
+
+do
+    local found, gameName, gameScript = false, nil;
+    for name,ids in next, games do
+        if table.find(ids, game.GameId) and files[name..'.lua'] then
+            found = true;
+            gameScript = files[name..'.lua'];
+            gameName = name;
+            break
+        end
+    end
+    if not found then
+        gameScript = files['universal.lua'];
+        gameName = 'universal';
+    end
+
+    print(found, gameName)
+
+    if gameScript ~= nil then
+        if isfile('ui_nxwr.lua') then
+            loadstring(readfile('ui_nxwr.lua'))();
+            library.title = 'Nxwr Projects | Project Delta GUI 0.1v - '..gameName;
+            library.gamename = gameName;
+            library:Init();
+            repeat task.wait() until library.hasInit == true
+            loadstring(gameScript)()
+        end
+    end
+end
